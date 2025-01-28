65956d17f3f9be5b64480ebb0ef756505c57a38c
EconLQ <zavliybogdan@gmail.com>
2025-01-28 17:25:38 +0200

    blog entity

---
+++ src/app/common/blog.ts
@@ -0,0 +1,12 @@
+export interface Blog {
+  id: number;
+  title: string;
+  body: string;
+  slug: string;
+  tags: string[];
+  published: boolean;
+  viewCount: number;
+  createdAt: Date; // or Date if you plan to parse it
+  updatedAt: Date; // or Date if you plan to parse it
+  author: string;
+}
