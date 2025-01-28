3d94f77a7e3b9e4de7e14cd2c34b4b64a7f568c5
EconLQ <zavliybogdan@gmail.com>
2025-01-28 17:27:39 +0200

    date converter

---
+++ src/app/utils/date-format.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat',
+  standalone: true,
+})
+export class DateFormatPipe implements PipeTransform {
+  transform(value: string | Date): string {
+    if (!value) return '';
+
+    const date = new Date(value);
+    const options: Intl.DateTimeFormatOptions = {
+      month: 'short',
+      day: 'numeric',
+    };
+    return date.toLocaleDateString('en-US', options).toUpperCase(); // example "JAN 27"
+  }
+}
