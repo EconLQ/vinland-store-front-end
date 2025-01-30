8b742c6efe41d9a686b0d9dc654ca03786daaba3
EconLQ <zavliybogdan@gmail.com>
2025-01-30 15:34:26 +0200

    moved logic from app to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BlogsComponent } from './components/blogs/blogs.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, BlogsComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'front-end';
-}
+export class AppComponent {}
