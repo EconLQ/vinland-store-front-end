014fe744588e71acda0870496e20e0b83a34761c
EconLQ <zavliybogdan@gmail.com>
2025-01-30 15:32:31 +0200

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'archive',
+    loadComponent: () =>
+      import('./components/blogs/blogs.component').then(
+        (m) => m.BlogsComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/about/about.component').then(
+        (m) => m.AboutComponent
+      ),
+  },
+];
