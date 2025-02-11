e89c72b6befbdaeedb9c62381036ffc2659d2ff1
EconLQ <zavliybogdan@gmail.com>
2025-02-11 21:13:24 +0200

    updated links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  loggedIn: boolean = this.authService.isLoggedIn;
+  constructor(private authService: AuthService) {}
 
+  logout() {
+    this.authService.signOut();
+  }
 }
