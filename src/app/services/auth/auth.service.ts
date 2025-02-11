944857dc3a797cabbb991fcad5c63dc151ff11f3
EconLQ <zavliybogdan@gmail.com>
2025-02-11 21:10:56 +0200

    created auth service for handling REST

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,13 +2,16 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../../common/login-response';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
 import { environment } from '../../../environments/environment';
+import { UserSignUpRequest } from '../../common/user-sign-up-request';
+import { log } from 'console';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  isLoggedIn: boolean = false;
   private authBaseUrl = `${environment.apiBaseUrl}/auth`;
   private headersOptions = {
     withCredentials: true, // include cookies with the request
@@ -20,39 +23,47 @@ export class AuthService {
 
   constructor(private httpClient: HttpClient, private rotuer: Router) {}
 
-  public login(loginData: { email: string; password: string }) {
+  public login(loginData: {
+    email: string;
+    password: string;
+  }): Observable<LoginResponse> {
     const payload = new HttpParams()
       .set('email', loginData.email)
       .set('password', loginData.password);
-    return this.httpClient
-      .post<LoginResponse>(
-        `${this.authBaseUrl}/sign-in`,
-        payload,
-        this.headersOptions
-      )
-      .subscribe(
-        (response: LoginResponse) => {
-          // this.tokenService.saveToken(response['access_token']);
-          this.rotuer.navigateByUrl('/');
-        }
-        // (error) => {
-        //   console.log('Error logging in: ', error);
-        //   console.log('Error status code: ', error.code);
-        // }
-      );
+    return this.httpClient.post<LoginResponse>(
+      `${this.authBaseUrl}/sign-in`,
+      payload,
+      this.headersOptions
+    );
   }
-  public register(signUpForm: NgForm) {
+  public register(signUpRequest: UserSignUpRequest): Observable<void> {
+    const payload = new HttpParams()
+      .set('username', signUpRequest.username)
+      .set('firstName', signUpRequest.firstName)
+      .set('lastName', signUpRequest.lastName)
+      .set('email', signUpRequest.email)
+      .set('password', signUpRequest.password);
+
+    return this.httpClient.post<void>(
+      `${this.authBaseUrl}/sign-up`,
+      payload,
+      this.headersOptions
+    );
+  }
+  public signOut() {
     return this.httpClient
-      .post<void>(
-        `${this.authBaseUrl}/sign-up`,
-        { ...signUpForm.value },
-        this.headersOptions
-      )
+      .post<string>(`${this.authBaseUrl}/logout`, null, {
+        withCredentials: true,
+      })
       .subscribe(
         (response) => {
+          console.log(response);
+          this.isLoggedIn = false;
           this.rotuer.navigateByUrl('/sign-in');
         },
-        (error) => console.log('Error signing up: ', error)
+        (error) => {
+          console.log('Error logging out: ', error);
+        }
       );
   }
 }
